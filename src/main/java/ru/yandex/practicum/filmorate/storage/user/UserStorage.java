ea2f29769c19262e025ca686c9ce6fc183b1403c
rabarcre <yylou@yandex.ru>
2025-04-14 13:51:31 +0300

    nichego1

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,7 +5,6 @@ import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 public interface UserStorage {
     Collection<User> findAllUsers();
